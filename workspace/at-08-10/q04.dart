662a572f8e87e021d1fab26a87eb52e8f2f3f23b
Henrique Abreu <164778108+abreuhenrique@users.noreply.github.com>
2025-10-08 20:33:53 -0300

    resolucao de questoes

---
+++ workspace/at-08-10/q04.dart
@@ -0,0 +1,10 @@
+import:'dart:io';
+void main(){
+    print("Digite um número referente a um dos mêses do ano");
+    int n = int.parse(stdin.readLineSync()!);
+
+    switch case(n){
+        case 1:
+            print()
+    }
+}
