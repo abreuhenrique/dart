3af3eb7a334cca60a6e35493822c11d7b7c19553
abreuhenrique <164778108+abreuhenrique@users.noreply.github.com>
2025-10-22 08:31:56 -0300

    laços de repetição em Dart

---
+++ workspace/atividades/01-for/vot_alun.dart
@@ -0,0 +1,11 @@
+/**
+ * Crie um cóigo que receba a quatidade de alunos
+ * Solicite o voto de cada aluno em dois candidatos
+ * e em seguida mostre a porcetagem de votos de cada um
+ * e quem venceu 100 % (votx/quantvots)
+ */
+import 'dart:io';
+void main(){
+  print('Digite a quantidades de alunos da turma: ');
+  int quant = int.parse(stdin.readLineSync()!);
+}
---
+++ workspace/atividades/02-while/desafio.dart
@@ -0,0 +1,13 @@
+/**
+ * Repetição -> While
+ * Ler a temp de cada dia da semana
+ * 
+ * Ao Final o laço(repetição)
+ * mostrar o dia da semana
+ *  C/ maior temp
+ *  C/ menor temp
+ */
+
+void main(){
+
+}
