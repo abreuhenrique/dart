80590bfa578b4b31a14681afe2daa6b92f902bd6
abreuhenrique <164778108+abreuhenrique@users.noreply.github.com>
2025-10-22 09:00:34 -0300

    desafio temp usando while

--- workspace/atividades/02-while/desafio.dart
+++ workspace/atividades/02-while/desafio.dart
@@ -7,7 +7,50 @@
  *  C/ maior temp
  *  C/ menor temp
  */
-
+import 'dart:io';
 void main(){
-
+  int x = 1;
+  String dia = "";
+  String diaMin = "";
+  String diaMax = "";
+  double min = 100;
+  double max = 0;
+  while (x <= 7){
+    switch (x) {
+      case 1:
+        dia = 'Domingo';
+      break;
+      case 2:
+        dia = 'Segunda';
+      break;
+      case 3:
+        dia = 'Terça';
+      break;
+      case 4:
+        dia = 'Quarta';
+      break;
+      case 5:
+        dia = 'Quinta';
+      break;
+      case 6:
+        dia = 'Sexta';
+      break;
+      case 7:
+        dia = 'Sábado';
+      break;
+    }
+  }
+    print('Digite a temperatura no dia: $dia');
+  double temp = double.parse(stdin.readLineSync()!);
+  print('Temperatura em $dia: $temp');
+  if(temp > max){
+    diaMax = dia;
+    max = temp;
+  }
+  if(temp < min){
+    diaMin = dia;
+    min = temp;
+  }
+  print('Dia com maior temperatura: $diaMax: $max');
+  print('Dia com menor temperatura: $diaMin: $min');
 }
